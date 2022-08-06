66ef09e71d6a808f2a0fde7faabe061cc5b3bf91
danielkezner <106866674+danielkezner@users.noreply.github.com>
2022-08-06 15:40:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const cmd = require('mineflayer-cmd').plugin
 const fs = require('fs');
 let rawdata = fs.readFileSync('config.json');
 let data = JSON.parse(rawdata);
-var lasttime = -1;
+var lasttime = 99999999999999999999999999999999;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var actions = [ 'left', 'right', 'left', 'right']
 var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
-var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
+var maxrandom = 15; // 0-15 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
 var nightskip = data["auto-night-skip"]
